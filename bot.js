668cef04192fdbb2e8e3ade631309505d194dede
codexuz <142141576+codexuz@users.noreply.github.com>
2023-10-29 00:54:01 +0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -19,51 +19,20 @@ bot.start((ctx)=>{
     },1000)    
 })
 
-
-
-// Listen for new chat members
 bot.on('new_chat_members', async (ctx) => {
-  try {
-    const messageInfo = await ctx.telegram.getMessage(
-      ctx.chat.id, // The chat ID where the message is
-      ctx.message.message_id // The message ID
-    );
-
-    if (messageInfo) {
-      // Delete the "user joined" message
-      await ctx.deleteMessage(messageInfo.message_id);
-      
-      // Send a welcome message to the new member
+  // Delete the "user joined" message
+  await ctx.deleteMessage(ctx.message.message_id);
+     // Send a welcome message to the new member
       ctx.reply(`👋 ${ctx.message.new_chat_members[0].first_name} guruhga xush kelibsiz!`);
-    } else {
-      console.error('Message not found.');
-    }
-  } catch (error) {
-    console.error('Error:', error);
-  }
 });
 
-// Listen for left chat members
 bot.on('left_chat_member', async (ctx) => {
-  try {
-    const messageInfo = await ctx.telegram.getMessage(
-      ctx.chat.id, // The chat ID where the message is
-      ctx.message.message_id // The message ID
-    );
-
-    if (messageInfo) {
-      // Delete the "user left" message
-      await ctx.deleteMessage(messageInfo.message_id);
-      
-      // You can also send a goodbye message or perform other actions here
+  // Delete the "user left" message
+  await ctx.deleteMessage(ctx.message.message_id);
+       // You can also send a goodbye message or perform other actions here
       ctx.reply(`🤚 Xayr, ${ctx.message.left_chat_member.first_name}!`);
-    } else {
-      console.error('Message not found.');
-    }
-  } catch (error) {
-    console.error('Error:', error);
-  }
 });
+
 
 
 bot.use(async (ctx, next) => {
